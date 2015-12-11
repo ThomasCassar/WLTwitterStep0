79909ec9970e9da17e4134a3cfc0331eed4d3398
ThomasCassar <thomas.cassar@isen-lille.fr>
2015-12-11 14:56:08 +0100

    ajout du gitignore et suppression des fichiers itel

--- app/src/main/java/worldline/ssm/rd/ux/wltwitter/async/RetrieveTweetsAsyncTask.java
+++ app/src/main/java/worldline/ssm/rd/ux/wltwitter/async/RetrieveTweetsAsyncTask.java
@@ -1,7 +1,39 @@
 package worldline.ssm.rd.ux.wltwitter.async;
 
+import android.os.AsyncTask;
+import android.text.TextUtils;
+
+import java.util.List;
+
+
+import worldline.ssm.rd.ux.wltwitter.helpers.TwitterHelper;
+import worldline.ssm.rd.ux.wltwitter.pojo.Tweet;
 /**
  * Created by cassar on 10/12/15.
  */
-public class RetrieveTweetsAsyncTask {
+
+public class RetrieveTweetsAsyncTask extends AsyncTask<String, Integer, List<Tweet>> {
+
+    public String appName = "TestTweet";
+
+    @Override
+    protected void onPostExecute(List<Tweet> tweets) {
+        super.onPostExecute(tweets);
+        int i;
+        for ( i=0 ; i< tweets.size() ; i++) {
+            System.out.println("[" +appName+"]" + tweets.get(i).text);
+        }
+    }
+
+    @Override
+    protected List<Tweet> doInBackground(String... params) {
+        if (TextUtils.isEmpty(params[0])){
+            return null;
+        }
+        else{
+            return TwitterHelper.getTweetsOfUser(params[0]);
+        }
+
+
+    }
 }
